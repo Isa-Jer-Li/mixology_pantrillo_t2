92f39f0dd8b0bfb6affe68767c5dada49d27b302
Isabelle <hatingusernames97@gmail.com>
2022-05-13 12:39:10 -0500

    added react

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cocktail: [],
+    }
+  }
+  render() {
+    return <>
+    <div className = 'mian-container'>
+      <form>
+      <div className = 'search-container'>
+
+      </div>
+      </form>
+
+
+
+    </div>
+
+    </>;
+  }
+
+}
+
+
+export default App;
+// mod stuff in this file
+// this is how you make app
